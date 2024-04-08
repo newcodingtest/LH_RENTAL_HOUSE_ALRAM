c2f7452f726e01168536bd3d66303c2778e4bb7d
jyyoon <pulpul8282@naver.com>
2024-04-08 16:50:40 +0900

    FIX: search api 스펙 변경

--- src/main/kotlin/org/alram/lh/rental/domain/ApiResponse.kt
+++ src/main/kotlin/org/alram/lh/rental/domain/ApiResponse.kt
@@ -3,14 +3,25 @@ package org.alram.lh.rental.domain
 import com.fasterxml.jackson.annotation.JsonProperty
 
 data class ApiResponse(
+   /**
+    * 공고게시일
+    * */
     @JsonProperty("PAN_NT_ST_DT")//PAN_NT_ST_DT
-   val startDT: String = "" ,
+    val postedDate: String = "" ,
     @JsonProperty("AIS_TP_CD_NM")
     val majorCategiry: String = "" ,
     @JsonProperty("CNP_CD_NM")
     val city: String = "" ,
+   /**
+    * 모집공고 시작일
+    * */
     @JsonProperty("PAN_DT")
-    val noticeStartDT: String = "" ,
+    val recruitStartDT: String = "" ,
+   /**
+    * 모집공고 종료일
+    * */
+    @JsonProperty("CLSG_DT")
+    val recruitEndDT: String = "" ,
     @JsonProperty("DTL_URL")
     val noticeUrl: String = "" ,
     @JsonProperty("PAN_NM")
